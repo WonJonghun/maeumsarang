94fbdb4f9f054f84f64099f695efdf4e4c3ecb42
WJH <whdgnswkd12@naver.com>
2026-06-10 14:11:27 +0900

    * 암호화 코드

--- src/main/java/com/example/mshintra/common/controller/JasyptEncryptTest.java
+++ src/main/java/com/example/mshintra/common/controller/JasyptEncryptTest.java
@@ -1,27 +1,27 @@
-//package com.example.mshintra.common.controller;
-//
-//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-//
-//public class JasyptEncryptTest {
-//
-//    public static void main(String[] args) {
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//        config.setPassword("");   여기 암호화 키
-//        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
-//        config.setKeyObtentionIterations("1000");
-//        config.setPoolSize("1");
-//        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-//        config.setStringOutputType("base64");
-//
-//        encryptor.setConfig(config);
-//
-//        String encrypted = encryptor.encrypt(""); 여기 암호화 할 코드
-//        String decrypted = encryptor.decrypt(encrypted);
-//
-//        System.out.println("encrypted = " + encrypted);
-//        System.out.println("decrypted = " + decrypted);
-//    }
-//}
+package com.example.mshintra.common.controller;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+
+public class JasyptEncryptTest {
+
+    public static void main(String[] args) {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("");   //여기 암호화 키
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
+
+        String encrypted = encryptor.encrypt(""); //여기 암호화 할 코드
+        String decrypted = encryptor.decrypt(encrypted);
+
+        System.out.println("encrypted = " + encrypted);
+        System.out.println("decrypted = " + decrypted);
+    }
+}
